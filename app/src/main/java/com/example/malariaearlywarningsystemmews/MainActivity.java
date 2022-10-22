36ae326704f1110f113c29c0cb259c5e69febf5c
Catalogo81 <peacematjato@gmail.com@gmail.com>
2022-10-22 13:54:20 +0200

    Retrieve User details on Main menu

--- app/src/main/java/com/example/malariaearlywarningsystemmews/MainActivity.java
+++ app/src/main/java/com/example/malariaearlywarningsystemmews/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.malariaearlywarningsystemmews;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,12 +11,19 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.malariaearlywarningsystemmews.classes.User;
 import com.example.malariaearlywarningsystemmews.login.Login;
 import com.example.malariaearlywarningsystemmews.register.Register;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -25,6 +33,11 @@ public class MainActivity extends AppCompatActivity {
     Button btnLogout;
 
     FirebaseAuth mAuth;
+
+    private FirebaseUser user;
+    private DatabaseReference reference;
+
+    private String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +56,39 @@ public class MainActivity extends AppCompatActivity {
                 mAuth.signOut();
                 startActivity(new Intent(MainActivity.this, Login.class));
 
+            }
+        });
+
+        //get user details from firebase
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        userID = user.getUid();
+
+        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                //User object
+                User userProfile = snapshot.getValue(User.class);
+
+                //check if user profile exists
+                if(userProfile != null)
+                {
+                    String name = userProfile.getName();
+                    String surname = userProfile.getSurname();
+                    String email = userProfile.getEmail();
+                    String number = userProfile.getPhoneNumber();
+
+                    //set the details to the view
+                    tvFullName.setText("Welcome, " + name + " " + surname);
+                    tvEmail.setText("Logged in as: " + email);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+                Toast.makeText(MainActivity.this, "Error: " + error.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
 
